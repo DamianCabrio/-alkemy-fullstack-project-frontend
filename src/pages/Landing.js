c51e9df01871ce8c4e4f90e4fbd0c4ed0c2aa19d
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-20 08:34:05 -0300

    Added basic landing page ➕

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,31 @@
-import logo from '../assets/images/logo.svg'
+import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+
+import Wrapper from '../styledWrappers/Landing';
 
 const Landing = () => {
   return (
-    <h4>Landing Page</h4>
-  )
-}
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="Tus Finanzas" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Mantén tus <span>finanzas</span> al dia
+          </h1>
+          <p>
+            En la economía actual es esencial mantener las finanzas a raya, "Tus
+            Finanzas" te permite hacerlo de forma fácil y ordenada. No dejes que
+            el dinero se te siga escapando de las manos, regístrate y empieza a
+            darle rienda a tus finanzas.
+          </p>
+          <button className='btn btn-hero'>Iniciar Sesión/Registrarse</button>
+        </div>
+        <img src={main} alt="Tus Finanzas" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
