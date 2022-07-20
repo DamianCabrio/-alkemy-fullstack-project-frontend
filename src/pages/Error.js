ae5b159eca35b795eff6d0ecf2b97db660cd05d3
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-20 16:07:13 -0300

    Added error page styles and structure ➕

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom';
+
+import img404 from '../assets/images/not-found.svg';
+import Wrapper from '../styledWrappers/Error';
+
 function Error() {
-  return <h1>Error</h1>;
+  return <Wrapper className='full-page'>
+    <div>
+      <img src={img404} alt='404' />
+      <h3>¡Oh! La pagina que busca no se encontró</h3>
+      <p>Puede que haya escrito mal la dirección o que la página que busca no exista.</p>
+      <Link to='/'>Ir a la página principal</Link>
+    </div>
+  </Wrapper>
 }
 export default Error;
