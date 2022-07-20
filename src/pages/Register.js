e00cb9aeedd3e343c24de3ef5cbf953b5fc9e835
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-20 16:30:50 -0300

    Added stub for login page ➕

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,47 @@
+import { useState, useEffect } from 'react';
+
+import { Logo } from '../components';
+import Wrapper from '../styledWrappers/Register';
+
+const initialState = {
+  name: '',
+  surname: '',
+  email: '',
+  password: '',
+  isLogin: true,
+};
+
 function Register() {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+      <Logo />
+      <h3>Iniciar sesión</h3>
+      <div className="form-row">
+        <label htmlFor='name' className='form-label'>Nombre</label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          className='form-input'
+          onChange={handleChange}
+          value={values.name}
+        />
+      </div>
+      <button type='submit' className='btn btn-block'>Enviar</button>
+      </form>
+    </Wrapper>
+  );
 }
 export default Register;
