b80f5088270b50512b576bb87b55966d36215ef0
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-23 11:21:33 -0300

    Added sidebar structure ➕

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,25 @@
+import { useAppContext } from '../contexts/app/appContext';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 import Wrapper from '../styledWrappers/Sidebar';
 
 function Sidebar() {
-  return <Wrapper>Sidebar</Wrapper>;
+  const { sidebarOpen } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          sidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 export default Sidebar;
