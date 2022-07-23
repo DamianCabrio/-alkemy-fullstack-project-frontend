deefa9e430d28c74753d14846db0b4f1abf539b5
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-22 21:52:54 -0300

    Added nested routes for dashboard ➕

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Landing, Error, Register } from './pages';
+import {
+  AddTransaction,
+  AllTransactions,
+  Profile,
+  Stats,
+  DashboardLayout,
+} from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<div>Dashboard</div>} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="operaciones" element={<AllTransactions />} />
+          <Route path="agregar" element={<AddTransaction />} />
+          <Route path="perfil" element={<Profile />} />
+        </Route>
         <Route path="/registro" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
--- src/pages/Dashboard/DashboardLayout.js
+++ src/pages/Dashboard/DashboardLayout.js
@@ -1,6 +1,15 @@
-function Layout() {
+import { Outlet, Link } from 'react-router-dom';
+import Wrapper from '../../styledWrappers/DashboardLayout';
+
+function DashboardLayout() {
   return (
-    <div>Layout Page</div>
-  )
+    <Wrapper>
+      <nav>
+        <Link to="/dashboard/operaciones">Operaciones</Link>
+        <Link to="/dashboard/agregar">Agregar</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
 }
-export default Layout
+export default DashboardLayout;
