72cd10862f28244d768d38e4545f809997401abf
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-22 22:52:40 -0300

    Created navbar structure ➕

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,45 @@
 import Wrapper from '../styledWrappers/Navbar';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../contexts/app/appContext';
+import Logo from './Logo';
 
 function Navbar() {
   return (
-    <Wrapper><h4>Navbar</h4></Wrapper>
-  )
+    <Wrapper>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log('toggle dropdown')}
+          >
+            <FaUserCircle />
+            Nombre
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log('cerrar sesión')}
+            >
+              Cerrar sesión
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
-export default Navbar
+export default Navbar;
