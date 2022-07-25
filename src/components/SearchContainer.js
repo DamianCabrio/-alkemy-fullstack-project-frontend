db50434658824c0403601e321b3dd401466ffd81
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-25 14:37:50 -0300

    Added basic structure for search form ➕

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,6 +1,44 @@
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../contexts/app/appContext';
+import Wrapper from '../styledWrappers/SearchContainer';
+
 function SearchContainer() {
+  const {
+    isLoading,
+    search,
+    searchType,
+    searchCategory,
+    sort,
+    handleInputChange,
+    clearFilters,
+    categoryOptions,
+    transactionTypes,
+    fetchCategoryOptions,
+    fetchTransactionTypes,
+  } = useAppContext();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (isLoading) return;
+    handleInputChange(e.target.name, e.target.value);
+  };
+
   return (
-    <div>SearchContainer</div>
-  )
+    <Wrapper>
+      <form className="form">
+        <h4>Búsqueda</h4>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            labelText="Buscar por descripción"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+            placeholder="Buscar"
+          />
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
-export default SearchContainer
+export default SearchContainer;
