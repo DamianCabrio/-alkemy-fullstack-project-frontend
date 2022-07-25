491c1734ca285722efa4cf5b7b3b2fcf2a7b7dd0
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-25 13:03:27 -0300

    Added AreaChart functionality ➕

--- src/components/AreaChart.js
+++ src/components/AreaChart.js
@@ -1,6 +1,39 @@
-function AreaChart() {
+import {
+  AreaChart as AreaChartComponent,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from 'recharts';
+
+function AreaChart({ data }) {
   return (
-    <div>AreaChart</div>
-  )
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChartComponent data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="label" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Legend />
+        <Area
+          type="monotone"
+          dataKey="total"
+          fill="#419D78"
+          name="Total de operaciones"
+          stackId="1"
+        />
+        <Area
+          type="monotone"
+          dataKey="total_amount"
+          fill="#592E83"
+          name="Total operado"
+          stackId="1"
+        />
+      </AreaChartComponent>
+    </ResponsiveContainer>
+  );
 }
-export default AreaChart
+export default AreaChart;
