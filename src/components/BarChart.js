74a6230786f6ef9b61e79c4a98ee62274579db4b
Damián Andrés Cabrio <soy@damiancabrio.com.ar>
2022-07-25 12:57:09 -0300

    Added BarChart funcionality ➕

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,6 +1,27 @@
-function BarChart() {
+import {
+  BarChart as BarChartComponent,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Legend
+} from 'recharts';
+
+function BarChart({data}) {
   return (
-    <div>BarChart</div>
-  )
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChartComponent data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="label" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total" fill="#419D78" name="Total de operaciones" />
+        <Bar dataKey="total_amount" fill="#592E83" name="Total operado" />
+      </BarChartComponent>
+    </ResponsiveContainer>
+  );
 }
 export default BarChart
--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import React, { useState } from 'react';
 
 import BarChart from './BarChart';
@@ -9,18 +10,26 @@ function ChartsContainer() {
   const [isShowBarChart, setIsShowBarChart] = useState(true);
   const { transactionStats } = useAppContext();
 
+  const data = transactionStats.groupByLastSixMonths.map((item) => {
+    const {month, year} = item;
+    const paddedMonth = month < 10 ? `0${month}` : month;
+    const formattedDate = DateTime.fromISO(
+      `${year}-${paddedMonth}`
+    ).toLocaleString({ month: 'long', year: 'numeric' });
+    return {
+      ...item,
+      label: formattedDate,
+    };
+  });
+
   return (
     <Wrapper>
       <h4>Operaciones Mensuales</h4>
       <button type="button" onClick={() => setIsShowBarChart(!isShowBarChart)}>
         {isShowBarChart ? 'Ver gráfico de áreas' : 'Ver gráfico de barra'}
       </button>
-      {isShowBarChart && (
-        <BarChart data={transactionStats.groupByLastSixMonths} />
-      )}
-      {!isShowBarChart && (
-        <AreaChart data={transactionStats.groupByLastSixMonths} />
-      )}
+      {isShowBarChart && <BarChart data={data} />}
+      {!isShowBarChart && <AreaChart data={data} />}
     </Wrapper>
   );
 }
